5cdc685c4bbb226c2c5c535c487f39797b10f490
Rakesh Bakolia <rakeshbakolia95@gmail.com>
2023-05-11 16:13:13 +0530

    chore: test flux

--- src/test/java/com/techi/springbootwebflux/MonoFluxTest.java
+++ src/test/java/com/techi/springbootwebflux/MonoFluxTest.java
@@ -1,13 +1,25 @@
 package com.techi.springbootwebflux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MonoFluxTest {
 
     @Test
     public void testMono(){
-        Mono<String> monoString = Mono.just("techi");
-        monoString.subscribe(System.out::println);
+        Mono<?> monoString = Mono.just("techi")
+                .then(Mono.error(new RuntimeException("Exception")))
+                .log();
+        monoString.subscribe(System.out::println, (e)-> System.out.println(e.getMessage()));
+    }
+
+    @Test
+    public void testFlux(){
+        Flux<String> fluxString = Flux.just("Spring", "Boot", "reactive", "mono")
+                .concatWithValues("flux")
+                .log();
+        fluxString.subscribe(System.out::println);
+
     }
 }
